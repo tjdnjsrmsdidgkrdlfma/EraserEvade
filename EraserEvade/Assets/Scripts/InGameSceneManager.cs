719d11e7277a32406b7d5cbb304357277ef4deef
tjdnjsrmsdidgkrID <tjdnjsrmsdidgkrdlapdlf@gmail.com>
2023-08-30 01:52:38 +0900

    Score system

--- EraserEvade/Assets/Scripts/InGameSceneManager.cs
+++ EraserEvade/Assets/Scripts/InGameSceneManager.cs
@@ -1,9 +1,10 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class InGameSceneManager : MonoBehaviour
 {
+    #region Obstacle
+
     [SerializeField] int obstacles_spawn_number_at_once;
     [SerializeField] float time_between_obstacles_spawn;
     [SerializeField] GameObject[] obstacles;
@@ -11,11 +12,26 @@ public class InGameSceneManager : MonoBehaviour
     float screen_x_half_size;
     float screen_y_half_size;
 
+    #endregion
+
+    #region Score
+
+    [SerializeField] int score_per_frame;
+    [SerializeField] TextMesh score_text;
+
+    int score;
+
+    #endregion
+
     void Start()
     {
         screen_x_half_size = Camera.main.orthographicSize * Camera.main.aspect;
         screen_y_half_size = Camera.main.orthographicSize;
+
+        score = 0;
+
         StartCoroutine(SpawnObstacles());
+        StartCoroutine(IncreaseScore());
     }
 
     IEnumerator SpawnObstacles()
@@ -32,6 +48,17 @@ public class InGameSceneManager : MonoBehaviour
                 Vector2 random_obstacle_spawn_position = new Vector2(Random.Range(-screen_x_half_size, screen_x_half_size), screen_y_half_size * 3);
                 Instantiate(obstacles[random_index], random_obstacle_spawn_position, Quaternion.identity);
             }
+        }
+    }
+
+    IEnumerator IncreaseScore()
+    {
+        while (true)
+        {
+            score += score_per_frame;
+            score_text.text = score.ToString();
+
+            yield return null;
         }
     }
 }
